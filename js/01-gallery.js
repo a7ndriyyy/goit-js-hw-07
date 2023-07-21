49019580eafbdf078aada8a148971e05ed34f618
a7ndriyyy <andrii.nastych@gmail.com>
2023-07-21 13:44:06 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+galleryList.addEventListener("click",onClickGallery);
+setGallerryHtml("beforeend", galleryElCreateMarkup(galleryItems));
+
+function onClickGallery(evt) {
+ evt.preventDefault();
+ if (evt.target.node.Name !== "IMG"){
+     return ;
+ }
+ openBigImages(evt.target.dataset.source)
+}
