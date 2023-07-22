d2aa452ea33b24781819b0451834b9da4d16cc25
a7ndriyyy <andrii.nastych@gmail.com>
2023-07-22 13:34:27 +0200

    correct

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const setGalleryHtml = galleryElCreateMarkup(galleryItems);
+
+function galleryElCreateMarkup(galleryItems){
+    return galleryItems
+    .map(({ preview,original,description}) => {
+       return `<li class = "gallery__item">
+              <a class ="gallery__link",
+              <img
+              class ="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+              />
+              </a>
+       </li>`;
+    })
+    .join("");
+}
+
+galleryList.insertAdjacentHTML('beforeend', setGalleryHtml);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
